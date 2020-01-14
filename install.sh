ed2dfda4523e525401389a0092f27aa472dbff4d
goncalogiga <58906040+goncalogiga@users.noreply.github.com>
2020-01-14 19:21:11 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 cd
+if [ -d ".ternest" ] ; then
+  echo "Reinstalling ternest..."
+  rm -rf .ternest
+else
+  echo "Installing ternest..."
 git clone https://github.com/goncalogiga/ternest.git
 mv ternest .ternest
 cd .ternest/
