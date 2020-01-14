82cce6f2d042b7d7f749029f805ea03ad9773886
goncalogiga <58906040+goncalogiga@users.noreply.github.com>
2020-01-14 22:02:30 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,4 +15,8 @@ mv ternest .ternest
 cd .ternest/
 gcc ternest.c -o ternest
 mv ternest ../.local/bin
+
+touch $HOME/.ternest/user/your_marks.txt
+touch $HOME/.ternest/cache/new_marks.txt
+
 echo "Ternest is now installed! Use ternest --config to set it up completly."
