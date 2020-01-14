77b9330f970baa7da8198e5309cf042e500839ca
goncalogiga <58906040+goncalogiga@users.noreply.github.com>
2020-01-14 19:12:42 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,9 +2,8 @@
 
 cd
 git clone https://github.com/goncalogiga/ternest.git
-#mv ternest .ternest
-#cd .ternest/
-#gcc -Werror -Wall ternest.c -o ternest
-#mv ternest ../.local/bin
-#echo "Testing ternest... (first use)"
-#ternest
+mv ternest .ternest
+cd .ternest/
+gcc ternest.c -o ternest
+mv ternest ../.local/bin
+echo "Testing ternest... (first use)"
