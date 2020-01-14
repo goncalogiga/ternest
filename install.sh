53e7ddc188bd7bc6d3a33b3c69839f93293f815a
goncalogiga <58906040+goncalogiga@users.noreply.github.com>
2020-01-14 19:45:07 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,25 +10,11 @@ else
 fi
 
 # GET TERNEST FROM GIT HUB AND MOVE EXE FILE INTO BIN FOLDER
-git clone https://github.com/goncalogiga/ternest.git
+sudo git clone https://github.com/goncalogiga/ternest.git
 mv ternest .ternest
 cd .ternest/
 gcc ternest.c -o ternest
 mv ternest ../.local/bin
 
-# GET THE USER'S CONFIG
-echo "Please configure ternest:"
-read "Your birthdate (DDMMYYYY): " birthdate
-read "Your student number: " student_id
-#read "Your prefered browser (Firefox or Chrome) :" browser
-
-sed -i '1\dataNaissance = $birthdate' ./.ternest/user/config.txt
-sed -i '2\codeEtudiant = $student_id' ./.ternest/user/config.txt
-sed -i '1\browser = Firefox' ./.ternest/user/config.txt
-
-# CREATE NECESSARY FOLDERS
-touch ./.ternest/user/your_marks.txt
-touch ./.ternest/cache/new_marks.txt
-
-# LAUNCH TERNEST FOR TESTING
-ternest
+# Launch the config initializer
+bash ./user/first_launch.sh
